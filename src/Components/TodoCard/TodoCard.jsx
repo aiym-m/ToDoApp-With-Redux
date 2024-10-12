0d2afef029831fd004346c0293ed5e252ad38bf4
Aiymkan <metinova.a@gmail.com>
2024-10-12 11:34:15 -0500

    TodoApp with redux

--- src/Components/TodoCard/TodoCard.jsx
+++ src/Components/TodoCard/TodoCard.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
-import "./TodoCard.css";
+import "./TodoCard.scss";
 
 function TodoCard({title, userId, completed, id, onToggle, onDelete}) {
 
 
   return (
     <div className='todo-card'>
-      
-     
-      <h2>{title}</h2>
-      <input type="checkbox" checked={completed } 
-      onChange={() => onToggle(id)}/>
+      <div className='title-check'>
+        <input type="checkbox" checked={completed } 
+      onChange={() => onToggle(id)} className=''/>
+        <h2>{title}</h2>
+      </div>
+      <div className='buttons'>
 			<h3>User ID: {userId}</h3>
 			<button onClick={() => onDelete(id)}>Delete</button>
+      </div>
     </div>
   )
 }
